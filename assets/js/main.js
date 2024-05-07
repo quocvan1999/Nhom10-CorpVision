6bf4fb2fd795d5e7056e77392b1f555c3bbb7284
QuocVan <lyquocvan1999@gmail.com>
2024-05-07 12:43:12 +0700

    cap nhat code nut back to to

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
 // // Shrink Navigation Menu
 const getElement = (selector) => {
-    return document.querySelector(selector);
-  };
+  return document.querySelector(selector);
+};
 
 window.onscroll = () => {
   scrollHeader();
+  scrollFunctionBackToTop();
 };
+
+// BACK TO TOP
+function scrollFunctionBackToTop() {
+  let backToTop = document.querySelector('#backToTop');
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTop.style.display = "flex";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
 
 const scrollHeader = () => {
   if (document.documentElement.scrollTop > 10) {
@@ -23,17 +35,19 @@ let btnCloseSearch = document.getElementById('btnCloseSearch');
 let searchContainer = document.getElementById('search__container');
 
 btnSearch.onclick = function () {
-    searchContainer.style.display = 'flex';
+  searchContainer.style.display = 'flex';
 }
 
 btnCloseSearch.onclick = function () {
-    searchContainer.style.display = 'none';
+  searchContainer.style.display = 'none';
 }
 
 // BUTTON DARK MODE
 let btnDarkMode = document.querySelector('#btnDarkMode');
 let bodyDark = document.querySelector('body');
 
-btnDarkMode.addEventListener('click',() => {
+btnDarkMode.addEventListener('click', () => {
   bodyDark.classList.toggle('darkMode');
 });
+
+
